31a33423b7f204df63bec90ebeaf9f8936dc1de6
ahdavis <ahdavis13@gmail.com>
2020-02-09 13:09:05 -0600

    Added Opcode enum

---
+++ src/codegen/Opcode.ts
@@ -0,0 +1,22 @@
+/*
+ * Opcode.ts
+ * Enumerates Aspera opcodes
+ * Created by Andrew Davis
+ * Created on 2/9/2020
+ * Licensed under the MIT License 
+ */
+
+// no imports
+
+/**
+ * The opcode segment of an Aspera instruction
+ */
+export enum Opcode {
+  /**
+   * No-op
+   */
+  NOP = 0x00,
+  // TODO: Add opcodes
+}
+
+// end of file

41f9e978e3ca5df24fc21b977e8b0bd1e2d93620
ahdavis <ahdavis13@gmail.com>
2020-02-09 13:08:51 -0600

    Added main export file

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * index.ts
+ * Main export file for aspera-isa
+ * Created by Andrew Davis
+ * Created on 2/9/2020
+ * Licensed under the MIT License 
+ */
+
+// exports
+export { Opcode } from './codegen/Opcode';
+
+// end of file

2a2d11ada259241533972ecb76afd51ec58d92ba
ahdavis <ahdavis13@gmail.com>
2020-02-21 04:45:47 -0600

    Added FlowOpcode typedef

---
+++ src/util/FlowOpcode.ts
@@ -0,0 +1,17 @@
+/*
+ * FlowOpcode.ts
+ * Defines a type that represents a program flow opcode
+ * Created by Andrew Davis
+ * Created on 2/21/2020
+ * Licensed under the MIT License
+ */
+
+// import
+import { Opcode } from '../codegen/Opcode';
+
+/**
+ * An [[Opcode]] that modifies program flow
+ */
+export type FlowOpcode = Opcode.JMP | Opcode.JN | Opcode.JNZ | Opcode.JP | Opcode.JZ | Opcode.CALL;
+
+// end of file
